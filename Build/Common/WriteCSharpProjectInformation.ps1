838be279d561c9cc647e689a9632ca569765bba5
finphie <19237170+finphie@users.noreply.github.com>
2023-01-09 23:50:14 +0900

    Create WriteCSharpProjectInformation.ps1

---
+++ Build/Common/WriteCSharpProjectInformation.ps1
@@ -0,0 +1,38 @@
+﻿[CmdletBinding(SupportsShouldProcess)]
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$projectName,
+
+    [Parameter(Mandatory)]
+    [ValidateSet('Console', 'Windows', 'Android', 'AspNet', 'BlazorWebAssembly')]
+    [string]$platform
+)
+
+[string]$outputFileName = 'repository.json'
+
+[Management.Automation.OrderedHashtable]$output = [Ordered]@{
+    'Projects' = [Ordered]@{
+        'Console' = @()
+        'Windows' = @()
+        'Android' = @()
+        'AspNet' = @()
+        'BlazorWebAssembly' = @()
+    }
+}
+
+if (Test-Path $outputFileName -PathType Leaf)
+{
+    $output = Get-Content $outputFileName | ConvertFrom-Json -AsHashtable
+}
+
+if ($output.Projects[$platform] -contains $projectName)
+{
+    Write-Verbose "Skip: $projectName"
+    exit
+}
+
+$output.Projects[$platform] += $projectName
+$output.Projects[$platform] = ($output.Projects[$platform] | Sort-Object) -as [string[]]
+
+$output | ConvertTo-Json | Out-File $outputFileName
